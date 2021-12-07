710a96d21d4dc5597e7550076c55f37bfce45715
AlaaShatat <73889806+AlaaShatat@users.noreply.github.com>
2021-12-07 22:42:54 +0200

    session and logout

--- backend/app.js
+++ backend/app.js
@@ -4,30 +4,18 @@ const User = require('./Models/user');
 const app = express();
 const bcrypt = require('bcrypt');
 const path = require('path');
- 
+const session = require('express-session');
+
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.use(express.urlencoded({extended: true}));
+app.use(session({secret:'this is  secret'}));
 
-app.get('/login',(req, res) =>{
-    res.render('login')
-} )
-app.post('/login',async (req, res) =>{
-    const {email, password} = req.body;
-    const user =await  User.findOne({email});
-   const validPAssword = await bcrypt.compare(password, user.password);
-   if (validPAssword){
-       res.send("yay WELCOME")
-   }
-   else{
-       res.send("incorrect")
-   }
-} )
+// register
 app.get('/register', (req, res) =>{
     res.render('register')
-})
-
+}) 
 app.post('/register', async(req,res) => {
     const { username, password, email} = req.body;
     const checkExist = await User.findOne({email});
@@ -43,9 +31,45 @@ app.post('/register', async(req,res) => {
         email: email
     })
     await user.save();
-    res.redirect('/login')
+    req.session.user_id = user._id;
+    res.redirect('/')
 }
 }) 
+
+// login 
+app.get('/login',(req, res) =>{
+    res.render('login')
+} )
+app.post('/login',async (req, res) =>{
+    const {email, password} = req.body;
+    const user =await  User.findOne({email});
+   const validPAssword = await bcrypt.compare(password, user.password);
+   if (validPAssword){
+       req.session.user_id = user._id;
+       res.render('home')
+   }
+   else{
+       res.redirect('/login')
+   }
+} )
+
+// log out 
+app.post('/logout', (req,res) => {
+    req.session.user_id = null;
+    res.redirect('/login')
+})
+
+app.get('/home',(req, res) =>{
+    if (!req.session.user_id)
+    {
+        res.redirect('/login')
+    }
+    else
+    res.render('home')
+})
+app.get('/',(req, res) =>{
+    
+})
 
 // connect to server
 app.listen(3000, ()=> {
