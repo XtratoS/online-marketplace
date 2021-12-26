c5f9fdec893aad746b437a93246e34bedb9ab201
Alaa-Ibrahim-Amer <69719564+Alaa-Ibrahim-Amer@users.noreply.github.com>
2021-12-26 20:09:56 +0200

    edit in profile

--- frontend/src/components/users/left/demo.js
+++ frontend/src/components/users/left/demo.js
@@ -4,8 +4,10 @@ import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
 import { CardActionArea } from '@mui/material';
+import { useSelector } from 'react-redux';
 
 export default function ActionAreaCard() {
+  const user = useSelector((state) => state.user.currentUser);
   return (
     <Card sx={{ maxWidth: 200 }}>
       <CardActionArea>
@@ -17,10 +19,11 @@ export default function ActionAreaCard() {
         />
         <CardContent>
           <Typography gutterBottom variant="h5" component="div">
-           user
+           {user.username}
           </Typography>
           <Typography variant="body2" color="text.secondary">
-          <div> Current cash balance: <span>122</span><span> LE</span></div>
+          <div> Current cash balance: <span>  {user.cash}</span><span> LE</span></div>
+        
           </Typography>
         </CardContent>
       </CardActionArea>
